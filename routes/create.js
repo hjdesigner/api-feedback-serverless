8a62f00c5da28b830dd5443d3c93ee68c0ee8a7b
Henrique Melanda <henrique.melanda@xpi.com.br>
2021-05-14 21:27:04 -0300

    Added type validation and empty field int the route to create feedback

--- routes/create.js
+++ routes/create.js
@@ -6,6 +6,42 @@ module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
 
+  if (typeof data.note !== 'number' || data.note === '') {
+    console.error('Validation note failed');
+    const response = {
+      statusCode: 400,
+      body: 'The note value must be a number and cannot be empty',
+    };
+    callback(null, response);
+  }
+
+  if (typeof data.brand !== 'number' || data.brand === '') {
+    console.error('Validation brand failed');
+    const response = {
+      statusCode: 400,
+      body: 'The brand value must be a number and cannot be empty',
+    };
+    callback(null, response);
+  }
+  
+  if (typeof data.feedback !== 'string' || data.feedback === '') {
+    console.error('Validation feedback failed');
+    const response = {
+      statusCode: 400,
+      body: 'The feedback value must be a string and cannot be empty',
+    };
+    callback(null, response);
+  }
+
+  if (typeof data.application !== 'string' || data.application === '') {
+    console.error('Validation application failed');
+    const response = {
+      statusCode: 400,
+      body: 'The application value must be a string and cannot be empty',
+    };
+    callback(null, response);
+  }
+
   const params = {
     TableName: 'feedback',
     Item: {
